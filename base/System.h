3c94490c5b3aea0945cb871be1cc544c3d0d3e1f
Lingfeng Yang <lfy@google.com>
2020-10-28 11:56:43 -0700

    Start building glestranslator
    Bug: 171711491
    Change-Id: I3541a40b428aff9a37146fa1ed13ffc0f2103b67

--- base/System.h
+++ base/System.h
@@ -8,9 +8,14 @@ void setEnvironmentVariable(const std::string& key, const std::string& value);
 bool isVerboseLogging();
 
 uint64_t getUnixTimeUs();
+uint64_t getHighResTimeUs();
 
 std::string getProgramDirectory();
 std::string getLauncherDirectory();
+
+uint64_t getFileSize(int fd, uint64_t* size);
+
+void sleepMs(uint64_t ms);
 
 } // namespace base
 } // namespace android
