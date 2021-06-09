b8b1d1d33879a20e48a68e73de47dbb190dd1753
Kaiyi Li <kaiyili@google.com>
2021-06-09 13:16:37 -0700

    vulkan: always enumerate all physical devices
    The original implementation could return unexpected VK_INCOMPLETE, and
    may not find the wanted device if the host Vulkan driver doesn't return
    that device when physicalDeviceCount is 1.
    
    Test: run the emulator on a laptop with 2 GPUs
    Bug: 190561902
    Change-Id: I4d6fdea6331b79799910bc20342b3f6f4d70660a

--- stream-servers/FrameBuffer.h
+++ stream-servers/FrameBuffer.h
@@ -17,6 +17,8 @@
 #define _LIBRENDER_FRAMEBUFFER_H
 
 #include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 #include <stdint.h>
 
 #include <functional>
@@ -784,6 +786,7 @@ class FrameBuffer {
     // UUIDs of physical devices for Vulkan and GLES, respectively.  In most
     // cases, this determines whether we can support zero-copy interop.
     uint8_t m_vulkanUUID[VK_UUID_SIZE];
-    uint8_t m_glesUUID[VK_UUID_SIZE];
+    uint8_t m_glesUUID[GL_UUID_SIZE_EXT];
+    static_assert(VK_UUID_SIZE == GL_UUID_SIZE_EXT);
 };
 #endif
