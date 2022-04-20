1b1610607d92a41bac8051b58016f909a1780688
Greg Schlomoff <gregschlom@google.com>
2022-04-19 17:03:35 -0700

    Adding script to auto-generate decoding logic for graphic API commands logged in minidumps.
    Test: manual testing for now, but I will add tests.
    
    BUG=223858461
    
    Change-Id: I4e467ff23a445198e2188a873f36a3ffc5ed30c6

--- scripts/print_gfx_logs/command_printer.py
+++ scripts/print_gfx_logs/command_printer.py
@@ -1,7 +1,9 @@
 import io
 import textwrap
-from typing import Dict
+from typing import Dict, Optional
 import vulkan_printer
+import opcodes
+import struct
 
 
 class CommandPrinter:
@@ -21,7 +23,11 @@ class CommandPrinter:
         """
 
         # Print out the command name
-        print("\n{}.{} - {}: ({} bytes)".format(self.stream_idx, self.cmd_idx, self.cmd_name(), self.original_size - 8))
+        print("\n{}.{} - {}: ({} bytes)".format(self.stream_idx, self.cmd_idx, self.cmd_name(),
+                                                self.original_size - 8))
+
+        if len(self.data.getbuffer()) == 0:
+            return
 
         pretty_printer = getattr(vulkan_printer, self.cmd_name(), None)
         if not pretty_printer:
@@ -33,7 +39,7 @@ class CommandPrinter:
             # Check that we processed all the bytes, otherwise there's probably a bug in the pretty printing logic
             if self.data.tell() != len(self.data.getbuffer()):
                 raise BufferError(
-                    "Not all data was decoded. Decoded {} bytes but expected {}".format(
+                    "Not all data was decoded. Decoded {} bytes but command had {}".format(
                         self.data.tell(), len(self.data.getbuffer())))
         except Exception as ex:
             print("Error while processing {}: {}".format(self.cmd_name(), repr(ex)))
@@ -43,7 +49,7 @@ class CommandPrinter:
 
     def cmd_name(self) -> str:
         """Returns the command name (e.g.: "OP_vkBeginCommandBuffer", or the opcode as a string if unknown"""
-        return vulkan_printer.opcodes.get(self.opcode, str(self.opcode))
+        return opcodes.opcodes.get(self.opcode, str(self.opcode))
 
     def print_raw(self):
         """Prints the command data as a hex bytes, as a fallback if we don't know how to decode it"""
@@ -52,16 +58,26 @@ class CommandPrinter:
         hex = ' '.join(["{:02x}".format(x) for x in self.data.getbuffer()])
         if truncated:
             hex += " [...]"
-        lines = textwrap.wrap(hex, width=16 * 3 + indent, initial_indent=' ' * indent, subsequent_indent=' ' * indent)
+        lines = textwrap.wrap(hex, width=16 * 3 + indent, initial_indent=' ' * indent,
+                              subsequent_indent=' ' * indent)
         for l in lines:
             print(l)
 
-    def read_int(self, num_bytes: int, signed: bool = False) -> int:
-        assert num_bytes == 4 or num_bytes == 8
-        buf = self.data.read(num_bytes)
-        if len(buf) != num_bytes:
-            raise EOFError("Unexpectly reached the end of the buffer")
-        return int.from_bytes(buf, byteorder='little', signed=signed)
+    def read_bytes(self, size: int):
+        buf = self.data.read(size)
+        if len(buf) != size:
+            raise EOFError("Unexpectedly reached the end of the buffer")
+        return buf
+
+    def read_int(self, size: int, signed: bool = False, big_endian: bool = False) -> int:
+        assert size in [1, 2, 4, 8], "Invalid size to read: " + str(size)
+        buf = self.read_bytes(size)
+        byte_order = 'big' if big_endian else 'little'
+        return int.from_bytes(buf, byteorder=byte_order, signed=signed)
+
+    def read_float(self) -> float:
+        buf = self.read_bytes(4)
+        return struct.unpack('f', buf)[0]
 
     def write(self, msg: str, indent: int):
         """Prints a string at a given indentation level"""
@@ -71,53 +87,118 @@ class CommandPrinter:
 
     def write_int(self,
                   field_name: str,
-                  num_bytes: int,
+                  size: int,
                   indent: int,
                   signed: bool = False,
-                  value: int = None):
-        """Reads the next 32 or 64 bytes integer from the data stream and prints it"""
-        if value is None:
-            value = self.read_int(num_bytes, signed)
-        self.write("{}: {}\n".format(field_name, value), indent)
+                  big_endian: bool = False,
+                  optional: bool = False,
+                  count: Optional[int] = None) -> Optional[int]:
+        """
+        Reads and prints integers from the data stream.
 
-    def write_enum(self, field_name: str, enum: Dict[int, str], indent: int, value: int = None):
-        """Reads the next 32-byte int from the data stream and prints it as an enum"""
-        if value is None:
-            value = self.read_int(4)
+        When reading a single int (ie: when count=None), returns the int that was read, otherwise
+        returns None.
+
+        size: size of the integer in bytes
+        indent: indentation level that we should write at
+        signed: whether to treat it as a signed or unsigned int
+        big_endian: whether to treat it as little endian or big endian
+        optional: if True, we will first read an 8 byte boolean value. If that value is false, we
+        will return without further reading.
+        count: how many integers to read, for repeated values.
+        """
+        if optional and self.check_null(field_name, indent):
+            return
+
+        # Write the field name
+        self.write("{name}: ".format(name=field_name), indent)
+
+        if count is not None:
+            values = ["0x{:x}".format(self.read_int(size, signed, big_endian)) for i in
+                      range(0, count)]
+            self.write("[{}]\n".format(", ".join(values)), indent=0)
+        else:
+            value = self.read_int(size, signed, big_endian)
+            # Print small values as decimal only, otherwise hex + decimal
+            format_str = ("{val}\n" if value < 10 else "0x{val:x} ({val})\n")
+            self.write(format_str.format(val=value), indent=0)
+            return value
+
+    def write_float(self, field_name: str, indent: int, count: Optional[int] = None):
+        if count is not None:
+            values = [str(self.read_float()) for i in range(0, count)]
+            self.write("{}: [{}]\n".format(field_name, ", ".join(values)), indent)
+        else:
+            self.write("{}: {}\n".format(field_name, self.read_float()), indent)
+
+    def write_enum(self, field_name: str, enum: Dict[int, str], indent: int) -> int:
+        """Reads the next 32-byte int from the data stream, prints it as an enum, and return it"""
+        value = self.read_int(4)
         self.write("{}: {} ({})\n".format(field_name, enum.get(value, ""), value), indent)
+        return value
+
+    def write_flags(self, field_name: str, enum: Dict[int, str], indent: int):
+        """Reads and prints Vulkan flags (byte masks)"""
+        flags = self.read_int(4)
+        flags_list = []
+        if flags != 0xffffffff:  # When the value is set to all flags, don't bother listing them all
+            for (value, flag) in enum.items():
+                if value & flags:
+                    flags_list.append(flag)
+        self.write("{}: {} (0x{:x})\n".format(field_name, " | ".join(flags_list), flags), indent)
 
     def write_stype_and_pnext(self, expected_stype: str, indent: int):
         """Reads and prints the sType and pNext fields found in many Vulkan structs, while also sanity checking them"""
-        stype = self.read_int(4)
-        self.write_enum("sType", vulkan_printer.VkStructureType, indent, value=stype)
-        if vulkan_printer.VkStructureType.get(stype) != expected_stype:
-            raise ValueError("Wrong sType while decoding data. Expected: " + expected_stype)
+        stype = self.write_enum("sType", vulkan_printer.VkStructureType, indent)
+        stype_str = vulkan_printer.VkStructureType.get(stype)
+        if stype_str != expected_stype:
+            raise ValueError("Wrong structure type. Expected: {}, got {} ({}) instead".format(
+                expected_stype, stype, stype_str))
 
-        pnext_size = self.read_int(4)
-        self.write_int("pNextSize", 4, indent, value=pnext_size)
+        pnext_size = self.write_int("pNext_size", 4, indent, big_endian=True)
         if pnext_size != 0:
-            raise NotImplementedError("Decoding structs with pNextSize > 0 not supported")
+            self.write_enum("ext type", vulkan_printer.VkStructureType, indent + 1)
+            raise NotImplementedError("Decoding structs with extensions is not supported")
 
-    def write_struct(self, field_name: str, struct_fn, indent: int):
-        """Reads and prints a struct, calling `struct_fn` to pretty-print it"""
-        self.write("{}:\n".format(field_name), indent)
-        struct_fn(self, indent + 1)
+    def check_null(self, field_name: str, indent) -> bool:
+        is_null = self.read_int(8, big_endian=True) == 0
+        if is_null:
+            self.write("{}: (null)\n".format(field_name), indent)
+        return is_null
 
-    def write_repeated(self,
-                       count_name: str,
-                       field_name: str,
-                       struct_fn,
-                       indent: int,
-                       pointer_name: str = None):
+    def write_struct(self, field_name: str, struct_fn, optional: bool, count: Optional[int],
+                     indent: int):
         """
-        Reads and prints repeated structs, with a 32-byte count field followed by the struct data.
-        If pointer_name is not None, reads an additional 64-bit pointer within the repeated block
-        before reading repeated data.
+        Reads and prints a struct, calling `struct_fn` to pretty-print it
+        optional: whether this is an optional structure. In this case, we will read an int64 first
+                  and skip the struct if the result is null.
+        count: how many times this is repeated. Pass None for non-repeated fields.
         """
-        count = self.read_int(4)
-        if pointer_name is not None:
-            self.write_int(pointer_name, 8, indent)
-        assert count < 1000, "count too large: {}".format(count)  # Sanity check that we haven't read garbage data
-        self.write_int(count_name, 4, indent, value=count)
-        for i in range(0, count):
-            self.write_struct("{} #{}".format(field_name, i), struct_fn, indent)
+        if optional and self.check_null(field_name, indent):
+            return
+
+        is_repeated = count is not None
+        for i in range(0, count if is_repeated else 1):
+            suffix = " #{}".format(i) if is_repeated else ""
+            self.write("{}{}:\n".format(field_name, suffix), indent)
+            struct_fn(self, indent + 1)
+
+    def write_string(self, field_name: str, size: Optional[int], indent: int):
+        """
+        Reads a null-terminated string from the stream.
+        size: if specified, reads up to this many characters
+        """
+        buf = b''
+        if size is not None:
+            buf = self.read_bytes(size)
+            buf = buf.rstrip(b'\x00')
+        else:
+            # Reads from the string one char at a time, until we find a null
+            # Not the most efficient way of doing this, but whatever
+            while True:
+                c = self.read_int(1)
+                if c == 0:
+                    break
+                buf += c
+
+        self.write("{}: \"{}\"\n".format(field_name, buf.decode('utf-8')), indent)
