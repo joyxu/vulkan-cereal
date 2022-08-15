faabeb264436d478dce73a3c0007f8b3b66d419b
William Ho <willho@google.com>
2022-08-15 15:32:27 -0700

    Upstream concrete metrics handling logic
    Bug: 242608281
    Test: Built and ran emulator
    Change-Id: I741d15599b01b7d7fa0dde3b5ad8a4acf4903bab

--- base/include/base/Metrics.h
+++ base/include/base/Metrics.h
@@ -82,7 +82,7 @@ using MetricEventType = std::variant<std::monostate, MetricEventFreeze, MetricEv
                                      MetricEventHang, MetricEventUnHang, GfxstreamVkAbort>;
 
 class MetricsLogger {
-public:
+   public:
     // Log a MetricEventType.
     virtual void logMetricEvent(MetricEventType eventType) = 0;
     // Virtual destructor.
@@ -90,12 +90,11 @@ public:
 
     // Callbacks to log events
     static void (*add_instant_event_callback)(int64_t event_code);
-    static void (*add_instant_event_with_descriptor_callback)(
-        int64_t event_code, int64_t descriptor);
-    static void (*add_instant_event_with_metric_callback)(
-        int64_t event_code, int64_t metric_value);
+    static void (*add_instant_event_with_descriptor_callback)(int64_t event_code,
+                                                              int64_t descriptor);
+    static void (*add_instant_event_with_metric_callback)(int64_t event_code, int64_t metric_value);
     // Crashpad will copy the strings, so these need only persist for the function call
-    static void(*set_crash_annotation_callback)(const char* key, const char* value);
+    static void (*set_crash_annotation_callback)(const char* key, const char* value);
 };
 
 std::unique_ptr<MetricsLogger> CreateMetricsLogger();
